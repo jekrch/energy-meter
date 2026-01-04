c62562f5fbde97e6cf56250439ff6604362a174e
colbs255 <colbs255@users.noreply.github.com>
2026-01-04 16:06:24 -0500

    flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [
+            pkgs.bun
+            pkgs.nodePackages.typescript-language-server
+          ];
+        };
+      });
+    };
+}
